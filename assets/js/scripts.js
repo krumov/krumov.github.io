9ace04c2436feaf0d0dc8fe1ab955921964065e9
krumov <krasin.krumov@gmail.com>
2017-03-05 14:15:29 +0200

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,25 @@
+jQuery(document).ready( function() {
+    function getWindowHeight() {
+        var winHeight = jQuery(window).height();
+        jQuery('header').css({ 'height': winHeight });
+    }
+
+    getWindowHeight();
+
+    jQuery('.rsvp-button').on('click', function (e) {
+        var aHrefName = jQuery(this).attr('href').substring(1);
+        var headingPos = jQuery('.wrapper').find('#' + aHrefName).offset().top;
+        jQuery('html, body').not(':animated').animate({scrollTop: headingPos - 80 }, 1000);
+        e.preventDefault();
+    });
+
+    jQuery("#kumsrules").on('click', function() {
+        jQuery('.kumsrules').addClass('active');
+        jQuery('.fader').addClass('active');
+    });
+
+    jQuery('.fader').on('click', function() {
+        jQuery('.kumsrules').removeClass('active');
+        jQuery('.fader').removeClass('active');
+    });
+});
